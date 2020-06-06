0ed1e3ed5257a6aad0a69beabf9d087fe7384edf
trcoo <trcooper1996@gmail.com>
2020-06-06 19:01:05 -0400

    Heavy refactoring. Main input loop for server and users have been combined into ManageUser in user.go. Command.go contains command structs that assign descriptive information to each console command. This makes it easier to iterate over them.

---
+++ command.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+)
+
+type command struct {
+	name        string
+	description string
+	format      string
+	adminOnly   bool
+	function    func(*user, []string)
+}
+
+var commandMap map[string]command
+
+func initializeCommands() {
+	commandMap = map[string]command{
+		"/all": {
+			name:        "/all",
+			description: "Sends a message to every user regardless of room.",
+			format:      "/all [message]",
+			adminOnly:   true,
+			function:    all,
+		},
+
+		"/create": {
+			name:        "/create",
+			description: "Creates a new room.",
+			format:      "/create [room name]",
+			adminOnly:   false,
+			function:    create,
+		},
+
+		"/switch": {
+			name:        "/switch",
+			description: "Switches to another room.",
+			format:      "/switch [room name]",
+			adminOnly:   false,
+			function:    switchRoom,
+		},
+
+		"/list": {
+			name:        "/list",
+			description: "Lists the name and number of users of each room.",
+			format:      "/list",
+			adminOnly:   false,
+			function:    list,
+		},
+
+		"/help": {
+			name:        "/help",
+			description: "Displays information on all commands.",
+			format:      "/help",
+			adminOnly:   false,
+			function:    help,
+		},
+	}
+}
+func all(_ *user, words []string) {
+	broadcastChannel <- strings.Join(words[1:], " ")
+}
+
+func create(u *user, words []string) {
+	if len(words) != 2 {
+		u.Write(fmt.Sprintf("Invalid format. Format is: \n%s.", commandMap[words[0]].format))
+		return
+	}
+
+	if _, exists := roomGroup[words[1]]; exists {
+		u.Write("Room already exists.")
+	} else {
+		r := newRoom(words[1], u.uName)
+		roomGroup[words[1]] = &r
+	}
+}
+
+func switchRoom(u *user, words []string) {
+	if len(words) != 2 {
+		u.Write(fmt.Sprintf("Invalid format. Format is: \n%s.", commandMap[words[0]].format))
+		return
+	}
+
+	if _, exists := roomGroup[words[1]]; exists {
+		u.currentRoom = words[1]
+		u.Write(fmt.Sprintf("Entering room %s", u.currentRoom))
+
+		f := openHistoryFile(u.currentRoom, false)
+		s := bufio.NewScanner(f)
+		//todo: Consider sending more than just one line per packet
+		for s.Scan() {
+			u.Write(s.Text())
+		}
+		roomGroup[u.currentRoom].users = append(roomGroup[u.currentRoom].users, u.uName)
+	} else {
+		u.Write("Room does not exist.")
+	}
+}
+
+func list(u *user, _ []string) {
+	//Todo: Send more than one line at a time
+	u.Write("Name\tUsers")
+	for _, r := range roomGroup {
+		u.Write(fmt.Sprintf("%s\t%d", r.name, len(r.users)))
+	}
+}
+
+func help(u *user, _ []string) {
+	var str strings.Builder
+	for _, c := range commandMap {
+		if !(c.adminOnly && !u.admin) {
+			str.WriteString(fmt.Sprintf("Name: %s\n\tDescription: %s\n\tFormat: %s", c.name, c.description, c.format))
+		}
+	}
+
+	bytes := []byte(str.String())
+	u.connection.Write(bytes)
+}
--- user.go
+++ user.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"net"
+	"os"
+	"strings"
 	"sync/atomic"
 )
 
@@ -11,6 +15,7 @@ type user struct {
 	connection      net.Conn
 	privateMessages chan []string
 	currentRoom     string
+	admin           bool
 }
 
 var userCounter uint32 = 0
@@ -19,4 +24,53 @@ func newUser(name string, con net.Conn) user {
 	c := userCounter
 	atomic.AddUint32(&c, 1)
 	return user{uName: name, uID: c, connection: con, privateMessages: make(chan []string), currentRoom: "main"}
+}
+
+func (u user) Write(data string) {
+	if u.uName == admin {
+		fmt.Printf("\r%s\n[%s] %s: ", data, u.currentRoom, u.uName)
+	} else {
+
+		byteData := []byte(fmt.Sprintf("\r%s\n[%s] %s: ", data, u.currentRoom, u.uName))
+
+		bytesWritten, err := u.connection.Write(byteData)
+		if err != nil {
+			fmt.Println(err.Error())
+		} else if bytesWritten != len(byteData) {
+			fmt.Println("Error, not all bytes written to client.")
+		}
+	}
+}
+
+func (u user) ManageUser() {
+	var reader bufio.Reader
+	if u.uName == admin {
+		fmt.Printf("\r[%s] %s: ", u.currentRoom, u.uName)
+		reader = *bufio.NewReader(os.Stdin)
+	} else {
+		u.connection.Write([]byte(fmt.Sprintf("\r[%s] %s: ", u.currentRoom, u.uName)))
+		reader = *bufio.NewReader(u.connection)
+	}
+
+	for {
+		text, _ := reader.ReadString('\n')
+		text = strings.TrimRight(text, "\n")
+
+		// If the first character is a '/', it is a command. Otherwise, it is a message
+		if text[0] == '/' {
+			words := strings.Split(text, " ")
+			if c, exists := commandMap[words[1]]; exists {
+				if c.adminOnly && !u.admin {
+					u.Write("This function is only available to admins.")
+				} else {
+					c.function(&u, words)
+				}
+			} else {
+				u.Write("Unknown command. Try /help")
+			}
+		} else {
+			messageChannel <- newMessage(text, u.uName, u.currentRoom)
+		}
+
+	}
 }
